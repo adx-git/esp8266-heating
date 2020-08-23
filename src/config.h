94a748ed1d2872df0f762fdc87695f039e1cc0b7
adx-git <git@nospam.suro-mail.de>
2020-08-23 13:08:38 +0200

    Added configuration file for environment specific data

---
+++ src/config.h
@@ -0,0 +1,15 @@
+/* 
+    Default configuration file, do not upload to repo
+*/
+
+#pragma once
+
+
+static const char SSID[] = "TestSSID";
+static const char PASS[] = "TestPASS_0815";
+static const char MQTT_USER[] = "user_test";
+static const char MQTT_PASSWORD[] = "password_test";
+static const IPAddress BROKER(192, 168, 1, 1);
+static const uint16_t PORT =  1883;
+
+#define PROTOCOL P300
